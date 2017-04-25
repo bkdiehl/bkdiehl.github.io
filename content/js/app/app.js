2633d9522db94199a15f6efb76db48756e090670
bkdiehl@gmail.com <bkdiehl@gmail.com>
2017-04-25 14:40:54 -0700

    replace pre elements with codepens

--- content/js/app/app.js
+++ content/js/app/app.js
@@ -21,24 +21,46 @@ angular.module('app', ['ngMaterial'])
 	];
 
 
-	var clickTabs = [ {id: "icon-hamburger"}, {id:"icon-lock"}, {id:"icon-arrow-rotating"}, {id:"icon-magnifying-glass"}, {id:"icon-folder"}, {id:"icon-recycling"}, {id:"icon-speaker"}, {id:"icon-envelope"}]
+	var clickTabs = [ 
+		{ id: "icon-hamburger", pen: "bWwQJZ" }, 
+		{ id:"icon-lock",pen:"qmqRRb" }, 
+		{ id:"icon-arrow-rotating", pen:"OmbpdL" }, 
+		{ id:"icon-magnifying-glass", pen:"rmWwVR" }, 
+		{ id:"icon-folder", pen:"VbmWjj" }, 
+		{ id:"icon-recycling", pen:"bWBREL" }, 
+		{ id:"icon-speaker", pen:"WjoOoy" }, 
+		{ id:"icon-envelope", pen:"JNbJEd" }];
 	$scope.clickDemos = clickTabs;
+	var clickIframes = [];
 	$scope.$watch('selectedClickIndex', function(current, old) {;
-		if(current + 1) {
-			if(clickTabs[current].schema === undefined)
-				clickTabs[current].schema = document.getElementById(clickTabs[current].id).innerHTML;
-		}
+		if(clickIframes.length < 1) {
+			clickIframes = document.querySelectorAll('.clickDemo');
+			for (var i = 0; i < clickTabs.length; i++) {
+				clickIframes[i].src = "//codepen.io/bkdiehl/embed/" + clickTabs[i].pen + "/?height=475&theme-id=0&default-tab=js&embed-version=2";		
+			}
+		}	
 	});
 
 
 
-	var hoverTabs = [ {id: "icon-share"}, {id: "icon-globe"}, {id: "icon-new-document"}, {id: "icon-link"}, {id: "icon-wrench"}, {id: "icon-wall"}, {id: "icon-mobile"}, {id: "icon-desktop"} ];
+	var hoverTabs = [ 
+		{ id: "icon-share", pen: "rmWzyW" },
+		{ id: "icon-globe", pen: "bWBryV" },
+		{ id: "icon-new-document", pen: "OmbxVV" },
+		{ id: "icon-link", pen: "JNbrda" },
+		{ id: "icon-wrench", pen: "KmNXdW" },
+		{ id: "icon-wall", pen: "bWBoEp" },
+		{ id: "icon-mobile", pen: "ZKBXWa" },
+		{ id: "icon-desktop", pen: "mmOBrb" }];
 	$scope.hoverDemos = hoverTabs;
+	var hoverIframes = [];
 	$scope.$watch('selectedHoverIndex', function(current, old) {
-		if(current + 1) {
-			if(hoverTabs[current].schema === undefined)
-				hoverTabs[current].schema = document.getElementById(hoverTabs[current].id).innerHTML;
-		}
+		if(hoverIframes.length < 1) {
+			hoverIframes = document.querySelectorAll('.hoverDemo');
+			for (var i = 0; i < hoverTabs.length; i++) {
+				hoverIframes[i].src = "//codepen.io/bkdiehl/embed/" + hoverTabs[i].pen + "/?height=475&theme-id=0&default-tab=js&embed-version=2";		
+			}
+		}	
 	});
 
 
